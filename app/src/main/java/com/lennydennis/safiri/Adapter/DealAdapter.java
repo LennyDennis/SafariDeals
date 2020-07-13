05d0528b07360fc390f57918447bee586c9c8a5e
LennyDennis <lennydennis@gmail.com>
2020-07-13 22:03:54 +0300

    Add a recyclerview to display the travel deals from firebase

---
+++ app/src/main/java/com/lennydennis/safiri/Adapter/DealAdapter.java
@@ -0,0 +1,116 @@
+package com.lennydennis.safiri.Adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.lennydennis.safiri.R;
+import com.lennydennis.safiri.TravelDeal;
+import com.lennydennis.safiri.Util.FirebaseUtil;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DealAdapter extends RecyclerView.Adapter<DealAdapter.ViewHolder> {
+
+    private static final String TAG = "Deal Adapter" ;
+    ArrayList<TravelDeal> mTravelDeals;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private ChildEventListener mChildEventListener;
+
+
+    public DealAdapter() {
+        FirebaseUtil.openFirebaseReference("traveldeals");
+        mFirebaseDatabase = FirebaseUtil.sFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.sDatabaseReference;
+        mTravelDeals = FirebaseUtil.sTravelDeals;
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                TravelDeal travelDeal = snapshot.getValue(TravelDeal.class);
+                Log.d(TAG, "onChildAdded:  "+travelDeal.getTitle());
+                travelDeal.setId(snapshot.getKey());
+                mTravelDeals.add(travelDeal);
+                notifyItemInserted(mTravelDeals.size()-1);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        mDatabaseReference.addChildEventListener(mChildEventListener);
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.deal_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        TravelDeal travelDeal = mTravelDeals.get(position);
+        holder.bind(travelDeal);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTravelDeals.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView dealTitle;
+        TextView dealDescription;
+        TextView dealPrice;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            dealTitle = itemView.findViewById(R.id.travel_title);
+            dealDescription = itemView.findViewById(R.id.travel_description);
+            dealPrice = itemView.findViewById(R.id.travel_price);
+        }
+
+        public void bind(TravelDeal travelDeal){
+            dealTitle.setText(travelDeal.getTitle());
+            dealDescription.setText(travelDeal.getDescription());
+            dealPrice.setText(travelDeal.getPrice());
+        }
+
+    }
+
+}
--- app/src/main/java/com/lennydennis/safiri/ListActivity.java
+++ app/src/main/java/com/lennydennis/safiri/ListActivity.java
@@ -1,69 +1,23 @@
 package com.lennydennis.safiri;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.lennydennis.safiri.Util.FirebaseUtil;
-
-import java.util.ArrayList;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import com.lennydennis.safiri.Adapter.DealAdapter;
 
 public class ListActivity extends AppCompatActivity {
-
-    ArrayList<TravelDeal> mTravelDeals;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mDatabaseReference;
-    private ChildEventListener mChildEventListener;
-    @BindView(R.id.safari_deals)
-    TextView travelDealsView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        ButterKnife.bind(this);
-
-        FirebaseUtil.openFirebaseReference("traveldeals");
-        mFirebaseDatabase = FirebaseUtil.sFirebaseDatabase;
-        mDatabaseReference = FirebaseUtil.sDatabaseReference;
-        mChildEventListener = new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                TravelDeal travelDeal = snapshot.getValue(TravelDeal.class);
-                travelDealsView.setText(travelDealsView.getText()+"\n"+travelDeal.getTitle());
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        };
-        mDatabaseReference.addChildEventListener(mChildEventListener);
+        RecyclerView recyclerView = findViewById(R.id.travel_deals);
+        DealAdapter dealAdapter = new DealAdapter();
+        recyclerView.setAdapter(dealAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
     }
 }
